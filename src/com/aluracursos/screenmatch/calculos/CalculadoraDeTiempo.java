12ad34924dc83144a9b8fcad827788dab46ac6ba
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-12-21 18:53:04 -0600

    Creating initial structure

---
+++ src/com/aluracursos/screenmatch/calculos/CalculadoraDeTiempo.java
@@ -0,0 +1,16 @@
+package com.aluracursos.screenmatch.calculos;
+
+import com.aluracursos.screenmatch.modelos.Titulo;
+
+public class CalculadoraDeTiempo {
+    private int tiempoTotal;
+
+    public int getTiempoTotal() {
+        return tiempoTotal;
+    }
+
+    public void incluye(Titulo titulo){
+        this.tiempoTotal += titulo.getDuracionEnMinutos();
+    }
+
+}
